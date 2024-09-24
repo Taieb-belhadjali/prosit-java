eb34c791b5017c225f2c7ea0896f0d5419bd8d03
Taieb-belhadjali <Taiebamine.Belhadjali@esprit.tn>
2024-09-24 16:08:48 +0100

    second

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,23 +1,76 @@
 public class Zoo {
-    Animal[] animals = new Animal[25];
+    Animal[] animals;
     String name;
     String city;
-    int nbrCages;
-    public Zoo(String name, String city, int nbrCages) {
+    final int NBR_CAGES = 25;  // Constante pour le nombre maximum de cages
+    int animalCount;
+
+    // Constructeur du zoo
+    public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
+        this.animals = new Animal[NBR_CAGES]; // Tableau de 25 animaux maximum
+        this.animalCount = 0;
     }
-    public void addAnimal(Animal animal, int index) {
-        if (index < 25) {
-            animals[index] = animal;
-        } else {
-            System.out.println("Impossible d'ajouter plus de 25 animaux.");
+
+    // Méthode pour ajouter un animal dans le zoo
+    public boolean addAnimal(Animal animal) {
+        if (animalCount >= NBR_CAGES) {
+            System.out.println("Le zoo est plein, impossible d'ajouter plus d'animaux.");
+            return false;
+        }
+
+        if (isAnimalInZoo(animal)) {
+            System.out.println("Cet animal existe déjà dans le zoo.");
+            return false;
+        }
+
+        animals[animalCount] = animal;
+        animalCount++;
+        return true;
+    }
+
+    // Méthode pour vérifier si un animal est déjà dans le zoo
+    public boolean isAnimalInZoo(Animal animal) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].name.equals(animal.name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isZooFull(){
+        return animalCount >= NBR_CAGES;
+    }
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.animalCount == z2.animalCount) {
+            return z1;
+        } else if (z2.animalCount > z1.animalCount) {
+            return z2;
+        }
+        else{
+            return z1;
         }
     }
+    // Méthode pour afficher les informations du zoo
     public void displayZooInfo() {
         System.out.println("Nom du zoo: " + name);
         System.out.println("Ville: " + city);
-        System.out.println("Nombre de cages: " + nbrCages);
+        System.out.println("Nombre maximal de cages: " + NBR_CAGES);
+        System.out.println("Nombre actuel d'animaux: " + animalCount);
+        System.out.println("le zoo est plain: " + isZooFull());
+    }
+
+    // Méthode pour afficher les animaux du zoo
+    public void displayAnimals() {
+        if (animalCount == 0) {
+            System.out.println("Le zoo ne contient aucun animal.");
+        } else {
+            System.out.println("Animaux dans le zoo:");
+            for (int i = 0; i < animalCount; i++) {
+                System.out.println((i + 1) + ". " + animals[i].name);
+            }
+        }
     }
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,11 +1,28 @@
 public class ZooManagement {
     public static void main(String[] args) {
+        // Création de deux zoos
+        Zoo zoo1 = new Zoo("Safari Park", "Paris");
+        Zoo zoo2 = new Zoo("Wildlife Reserve", "Lyon");
+
+        // Création de quelques animaux
         Animal lion = new Animal("Felidae", "Lion", 5, true);
-        Zoo myZoo = new Zoo("Safari Park", "Paris", 15);
+        Animal tiger = new Animal("Felidae", "Tigre", 4, true);
+        Animal bear = new Animal("Ursidae", "Ours", 7, true);
 
-        myZoo.addAnimal(lion, 0);
+        // Ajout d'animaux dans zoo1
+        zoo1.addAnimal(lion);
+        zoo1.addAnimal(tiger);
 
-        myZoo.displayZooInfo();
-        lion.displayAnimalInfo();
+        // Ajout d'animaux dans zoo2
+        zoo2.addAnimal(bear);
+        zoo2.addAnimal(new Animal("Felidae", "Panther", 3, true));
+        zoo2.addAnimal(new Animal("Canidae", "Loup", 6, true));
+
+        // Comparaison des deux zoos
+        Zoo biggerZoo = Zoo.comparerZoo(zoo1, zoo2);
+
+        // Affichage du zoo avec le plus d'animaux
+        System.out.println("\nLe zoo avec le plus d'animaux est :");
+        biggerZoo.displayZooInfo();
     }
 }
